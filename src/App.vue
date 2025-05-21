cd4e4bce1956fd21400a60405af9f8275b9a9788
kangjinseok <easyevergreen@naver.com>
2025-05-21 10:07:31 +0900

    start

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import Payments from './components/Payments.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <Payments />
+<!--  <header>-->
+<!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
 
-  <main>
-    <TheWelcome />
-  </main>
+<!--    <div class="wrapper">-->
+<!--      <HelloWorld msg="You did it!" />-->
+<!--    </div>-->
+<!--  </header>-->
+
+<!--  <main>-->
+<!--    <TheWelcome />-->
+<!--  </main>-->
 </template>
 
 <style scoped>
