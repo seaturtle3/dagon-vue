b4f48c4510ca4a070d58ae5f4c1222580db73eef
JJang_ez <dmswjd9866@gmail.com>
2025-06-03 15:06:57 +0900

    폴더정리

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/home/Home.vue'
 import Product from '@/views/product/Product.vue'
+import FishingCenter from "@/views/fishing-center/FishingCenter.vue";
 import SeaFishing from '@/views/sea-fishing/SeaFishing.vue'
 import FreshwaterFishing from '@/views/freshwater-fishing/FreshwaterFishing.vue'
-import Community from '@/views/community/Community.vue'
-import CustomerService from '@/views/customer-service/CustomerService.vue'
-import FishingCenter from "@/views/fishing-center/FishingCenter.vue";
+import CustomerService from '@/views/support/customer-service/CustomerService.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/fishing-center', component: FishingCenter },
     { path: '/sea-fishing', component: SeaFishing },
     { path: '/freshwater-fishing', component: FreshwaterFishing },
-    { path: '/community', component: Community },
     { path: '/customer-service', component: CustomerService },
     { path: '/products', component: Product },
 ]
