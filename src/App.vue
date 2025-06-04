a230f5d92d55f78cfeb11741d9b8bb1bdd922475
kangjinseok <easyevergreen@naver.com>
2025-06-04 16:35:37 +0900

    form 진행 중...3

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
-import Payments from "./views/payments/Payments.vue";
+import Payments from "./views/payments/Payment.vue";
 import Reservation from "./views/reservation/ReservationForm.vue";
-import InquiryForm from './views/inquiry/InquiryForm.vue';
+import InquiryForm from '@/views/inquiries/InquiryForm.vue';
 </script>
 
 <template>
-  <Payments />
-  <Reservation />
-  <InquiryForm />
   <div class="d-flex flex-column min-vh-100">
     <HeaderNav />
     <div class="router flex-grow-1" style="padding-top: 10%;">
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import InquiryForm from '@/views/inquiry/InquiryForm.vue';
-import Payments from '@/views/Payments/PaymentsForm.vue';
-import ReservationForm from "@/views/reservation/ReservationForm.vue";
+import InquiryForm from '@/views/inquiries/Inquiry.vue';
+// import Payments from '@/views/Payments/Payment.vue';
+import Reservation from "@/views/reservation/ReservationForm.vue";
+import Home from "@/views/home/Home.vue";
+import FishingCenter from "@/views/fishing-center/FishingCenter.vue";
+import SeaFishing from "@/views/sea-fishing/SeaFishing.vue";
+import FreshwaterFishing from "@/views/freshwater-fishing/FreshwaterFishing.vue";
+import CustomerService from "@/views/support/customer-service/CustomerService.vue";
+import Product from "@/views/product/Product.vue";
 
 
 const routes = [
-    {
-        path: '/inquiry/form',
-        name: 'InquiryForm',
-        component: InquiryForm
-    },
-    {
-        path: '/payments/from',
-        name: 'PaymentsForm',
-        component: () => import("@/views/payments/Payments.vue")
-    },
-    {
-        path: '/reservation/form',
-        name: 'ReservationForm',
-        component: ReservationForm
-    }
-];
+    { path: '/', component: Home },
+    { path: '/fishing-center', component: FishingCenter },
+    { path: '/sea-fishing', component: SeaFishing },
+    { path: '/freshwater-fishing', component: FreshwaterFishing },
+    { path: '/customer-service', component: CustomerService },
+    { path: '/products', component: Product },
+    { path: '/payments', component: Payment },
+    { path: '/inquiries', component: Inquiry },
+    { path: '/reservation', component: ReservationForm },
+]
 
 const router = createRouter({
     history: createWebHistory(),
