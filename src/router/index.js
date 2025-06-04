40a8a34a4957ba29558ce53a523dead35083443b
kangjinseok <easyevergreen@naver.com>
2025-06-04 16:45:46 +0900

    form 진행 중...4

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import InquiryForm from '@/views/inquiries/Inquiry.vue';
-// import Payments from '@/views/Payments/Payment.vue';
-import Reservation from "@/views/reservation/ReservationForm.vue";
-import Home from "@/views/home/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/home/Home.vue'
+import Product from '@/views/product/Product.vue'
 import FishingCenter from "@/views/fishing-center/FishingCenter.vue";
-import SeaFishing from "@/views/sea-fishing/SeaFishing.vue";
-import FreshwaterFishing from "@/views/freshwater-fishing/FreshwaterFishing.vue";
-import CustomerService from "@/views/support/customer-service/CustomerService.vue";
-import Product from "@/views/product/Product.vue";
-
+import SeaFishing from '@/views/sea-fishing/SeaFishing.vue'
+import FreshwaterFishing from '@/views/freshwater-fishing/FreshwaterFishing.vue'
+import CustomerService from '@/views/support/customer-service/CustomerService.vue'
 
 const routes = [
     { path: '/', component: Home },
@@ -17,9 +13,6 @@ const routes = [
     { path: '/freshwater-fishing', component: FreshwaterFishing },
     { path: '/customer-service', component: CustomerService },
     { path: '/products', component: Product },
-    { path: '/payments', component: Payment },
-    { path: '/inquiries', component: Inquiry },
-    { path: '/reservation', component: ReservationForm },
 ]
 
 const router = createRouter({
