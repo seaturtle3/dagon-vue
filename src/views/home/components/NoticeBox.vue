71ceafbc73c2d3457e985cca0e8b570559b4eb11
seaturtle3 <fttf136@gmail.com>
2025-06-05 11:30:00 +0900

    공지사항 수정

--- src/views/home/components/NoticeBox.vue
+++ src/views/home/components/NoticeBox.vue
@@ -5,14 +5,7 @@ export default {
   name: 'NoticeBox',
   data() {
     return {
-      notices: [
-        { id: 1, title: '공지사항 테스트 1' },
-        { id: 2, title: '공지사항 테스트 2' },
-        { id: 3, title: '공지사항 테스트 3' },
-        { id: 4, title: '공지사항 테스트 4' },
-        { id: 5, title: '공지사항 테스트 5' },
-        { id: 6, title: '공지사항 테스트 6' }
-      ],
+      notices: [],
       search: {
         keyword: '',
         type: '',
