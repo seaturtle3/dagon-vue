0606ca15286101ede456d1f7bb6ea5dc038d1f36
kangjinseok <easyevergreen@naver.com>
2025-06-09 11:01:03 +0900

    payment 진행 중...1

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,8 @@ import Sea from '@/views/fishing-filter/Sea.vue'
 import Freshwater from '@/views/fishing-filter/Freshwater.vue'
 import CustomerService from '@/views/support/customer-service/CustomerService.vue'
 import ProductDetail from '@/views/community/fishing-center/ProductDetail.vue'
-import Inquiry from '@/views/inquiries/Inquiry.vue'
-import Payment from '@/views/reservation/Payment.vue'
-import PaymentsList from '@/views/reservation/PaymentList.vue';
-import inquiry from '@/views/inquiries/Inquiry.vue'
-import Payment from '@/views/payments/Payment.vue'
+// import inquiry from '@/views/inquiries/Inquiry.vue'
+// import Payment from '@/views/payments/Payment.vue'
 import Reservation from '@/views/reservation/Reservation.vue'
 import login from "@/views/auth/login/Login.vue";
 import Community from "@/views/community/Community.vue";
@@ -32,12 +29,8 @@ const routes = [
     { path: '/community', component: Community },
     { path: '/customer-service', component: CustomerService },
     { path: '/products', component: Product },
-    { path: '/fishing-center/product/:id', component: ProductDetail, name: 'ProductDetail'},
-    { path: '/inquiries', component: () => import('@/views/inquiries/Inquiry.vue')},
-    { path: '/payments', component: Payment },
-    { path: '/pay/list', component: PaymentsList },
-    { path: '/reservation', component: Reservation },
     { path: '/inquiry', component: () => import('@/views/inquiries/Inquiry.vue')},
+    // { path: '/payments', component: Payment },
     { path: '/reservation', component: Reservation },
     { path: '/notice', component: () => import('@/views/support/notice/NoticeList.vue') }, // 비동기
     { path: '/support/notice/:id', component: () => import('@/views/support/notice/NoticeDetail.vue'),
