298259072d2e7c019e3b357b6532a09aef0ae81f
packjunha <asd020712a@gmail.com>
2025-06-10 16:29:29 +0900

    파트너 신청

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,10 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import AdminLayout from '../views/admin/AdminLayout.vue'
-import Dashboard from '../views/admin/components/Dashboard.vue'
-import Members from '../views/admin/components/Members.vue'
-import Partners from '../views/admin/components/Partners.vue'
-import PartnerApplications from '../views/admin/components/PartnerApplications.vue'
-import Reservations from '../views/admin/components/Reservations.vue'
-import Notifications from '../views/admin/components/Notifications.vue'
-import Reports from '../views/admin/components/Reports.vue'
-import Events from '../views/admin/components/Events.vue'
-import Notices from '../views/admin/components/Notices.vue'
-import FAQ from '../views/admin/components/FAQ.vue'
-import Inquiries from '../views/admin/components/Inquiries.vue'
-import Logout from '../views/admin/components/Logout.vue'
-import AdminLogin from '../views/auth/adminlogin/AdminLogin.vue'
-import AdminRegister from '../views/auth/adminregister/AdminRegister.vue'
-import MemberDetail from '@/views/admin/components/MemberDetail.vue'
 
 const routes = [
     {path: '/', component: () => import('@/views/home/Home.vue')},
     {path: '/fishing-center', component: () => import('@/views/community/fishing-center/FishingCenter.vue')},
-    {
-        path: '/fishing-center/product/:id',
-        component: () => import('@/views/community/fishing-center/ProductDetail.vue'),
-        name: 'ProductDetail'
-    },
+    {path: '/fishing-center/product/:id', component: () => import('@/views/community/fishing-center/ProductDetail.vue')},
     {path: '/products', component: () => import('@/views/product/Product.vue')},
     {path: '/sea', component: () => import('@/views/fishing-filter/Sea.vue')},
     {path: '/freshwater', component: () => import('@/views/fishing-filter/Freshwater.vue')},
@@ -33,19 +14,15 @@ const routes = [
     {path: '/payments', component: () => import('@/views/order/payment/Payment.vue')},
     {path: '/reservation', component: () => import('@/views/order/reservation/Reservation.vue')},
     {path: '/notice', component: () => import('@/views/support/notice/NoticeList.vue')},
-    {
-        path: '/support/notice/:id', component: () => import('@/views/support/notice/NoticeDetail.vue'),
-        props: true
-    },
+    {path: '/support/notice/:id', component: () => import('@/views/support/notice/NoticeDetail.vue'), props: true},
     {path: '/login', component: () => import('@/views/auth/login/Login.vue')},
     {path: '/register', component: () => import('@/views/auth/register/components/RegisterForm.vue')},
-    {path: '/admin/login', component: AdminLogin},
-    {path: '/admin/register', component: AdminRegister},
+    {path: '/admin/login', component: () => import('@/views/auth/adminlogin/AdminLogin.vue')},
+    {path: '/admin/register', component: () => import('@/views/auth/adminregister/AdminRegister.vue')},
     {
         path: '/mypage',
         component: () => import('@/views/pages/my-page/MyPageView.vue'),
         redirect: '/mypage/profile',
-        name: 'mypage',
         children: [
             {path: 'profile', component: () => import('@/views/pages/my-page/components/ProfileView.vue')},
             {path: 'password', component: () => import('@/views/pages/my-page/components/PasswordView.vue')},
@@ -53,93 +30,27 @@ const routes = [
             {path: 'reservations', component: () => import('@/views/pages/my-page/components/ReservationsView.vue')},
             {path: 'notifications', component: () => import('@/views/pages/my-page/components/NotificationsView.vue')},
             {path: 'withdrawal', component: () => import('@/views/pages/my-page/components/WithdrawalView.vue')},
-            {path: 'inquiries', name: 'mypage-inquiries',component: () => import('@/views/pages/my-page/components/InquiriesView.vue')},
+            {path: 'inquiries', component: () => import('@/views/pages/my-page/components/InquiriesView.vue')}
         ]
     },
     {
         path: '/admin',
         component: AdminLayout,
         children: [
-            {
-                path: '',
-                redirect: '/admin/dashboard'
-            },
-            {
-                path: 'dashboard',
-                name: 'Dashboard',
-                component: Dashboard,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'members',
-                name: 'Members',
-                component: Members,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'partners',
-                name: 'Partners',
-                component: Partners,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'partner-applications',
-                name: 'PartnerApplications',
-                component: PartnerApplications,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'reservations',
-                name: 'Reservations',
-                component: Reservations,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'notifications',
-                name: 'Notifications',
-                component: Notifications,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'reports',
-                name: 'Reports',
-                component: Reports,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'events',
-                name: 'Events',
-                component: Events,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'notices',
-                name: 'Notices',
-                component: Notices,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'faq',
-                name: 'FAQ',
-                component: FAQ,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'inquiries',
-                name: 'Inquiries',
-                component: Inquiries,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'logout',
-                name: 'Logout',
-                component: Logout,
-                meta: { requiresAuth: true }
-            },
-            {
-                path: 'member/:uid',
-                component: MemberDetail
-            }
+            {path: '', redirect: '/admin/dashboard'},
+            {path: 'dashboard', component: () => import('@/views/admin/components/Dashboard.vue'), meta: {requiresAuth: true}},
+            {path: 'members', component: () => import('@/views/admin/components/Members.vue'), meta: {requiresAuth: true}},
+            {path: 'partners', component: () => import('@/views/admin/components/Partners.vue'), meta: {requiresAuth: true}},
+            {path: 'partner-applications', component: () => import('@/views/admin/components/PartnerApplications.vue'), meta: {requiresAuth: true}},
+            {path: 'reservations', component: () => import('@/views/admin/components/Reservations.vue'), meta: {requiresAuth: true}},
+            {path: 'notifications', component: () => import('@/views/admin/components/Notifications.vue'), meta: {requiresAuth: true}},
+            {path: 'reports', component: () => import('@/views/admin/components/Reports.vue'), meta: {requiresAuth: true}},
+            {path: 'events', component: () => import('@/views/admin/components/Events.vue'), meta: {requiresAuth: true}},
+            {path: 'notices', component: () => import('@/views/admin/components/Notices.vue'), meta: {requiresAuth: true}},
+            {path: 'faq', component: () => import('@/views/admin/components/FAQ.vue'), meta: {requiresAuth: true}},
+            {path: 'inquiries', component: () => import('@/views/admin/components/Inquiries.vue'), meta: {requiresAuth: true}},
+            {path: 'logout', component: () => import('@/views/admin/components/Logout.vue'), meta: {requiresAuth: true}},
+            {path: 'member/:uid', component: () => import('@/views/admin/components/MemberDetail.vue'), meta: {requiresAuth: true}}
         ]
     }
 ]
