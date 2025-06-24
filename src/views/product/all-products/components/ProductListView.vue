adcd9dd3960aa66e7dba348ea6134dd69bf3f71f
seaturtle3 <fttf136@gmail.com>
2025-06-24 16:36:40 +0900

    선박 등록 폼 수정 중

--- src/views/product/all-products/components/ProductListView.vue
+++ src/views/product/all-products/components/ProductListView.vue
@@ -25,7 +25,7 @@ function goToProductForm() {
   <div class="container my-4">
 
     <div class="d-flex justify-content-between align-items-center mb-5">
-      <h4 class="m-0">모든 배 리스트</h4>
+      <h4 class="m-0">모든 선박 리스트</h4>
       <button
           type="button"
           class="btn btn-secondary"
