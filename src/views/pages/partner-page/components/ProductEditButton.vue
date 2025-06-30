4db1fafc4428ae700c7d7e81b6d9cdb6374d67be
plusmail <plus4957@gmail.com>
2025-06-30 20:41:17 +0900

    파트너에서 제품 수정 작업

---
+++ src/views/pages/partner-page/components/ProductEditButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import {useProductFormStore} from "@/store/product/all-products/useProductFormStore.js";
+
+const props = defineProps({
+  product: {
+    type: Object,
+    required: true
+  }
+});
+
+const router = useRouter();
+
+// 상품 수정 페이지로 이동
+function editProduct() {
+  const store = useProductFormStore();
+  store.setForm(props.product); // props에서 직접 꺼내 쓰기
+  router.push(`/partner/products/edit/${props.product.prodId}`);
+}
+
+console.log("넘기는 product 확인:", props.product);
+</script>
+
+<template>
+  <button @click="editProduct" class="btn btn-primary">
+    <i class="fas fa-edit"></i> 수정
+  </button>
+</template>
